6649f7a3ea353d7d07a21fc1d23dffee54333ae9
Rahul Yadav <hitece19rahul85@gmail.com>
2025-01-04 12:38:52 +0530

    first question

---
+++ 52_Maximum_Subarray.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int max=INT_MIN;
+        int prt1=0;
+        int prt2=0;
+        int sum=0;
+
+        while(prt1<nums.size() && prt2<nums.size()){
+            sum+=nums[prt2];
+            if(sum>max)max=sum;
+            if(sum<0){
+                sum-nums[prt1];
+                prt1=prt2;
+                prt2++;
+                sum=0;
+            }else{
+                prt2++;
+            }
+        }
+        return max;
+        
+    }
+};

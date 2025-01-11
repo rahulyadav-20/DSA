7cdef452c4fe7ae4e29c296304bb32629571a5a0
Rahul Yadav <hitece19rahul85@gmail.com>
2025-01-11 23:45:07 +0530

    Trapping_rain_water

--- Two pointer/42. Trapping Rain Water/Trapping_rain_water.java
+++ Two pointer/42. Trapping Rain Water/Trapping_rain_water.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 class Solution {
     //Time Complexity: O(n)
     //Space Complexity: O(n)
+    //Approach: Storing the left max and right max in two arrays and then taking the minimum of the two and subtracting the height[i] from it.
 
     public int trap(int[] height) {
         int water = 0;
@@ -37,6 +38,41 @@ class Solution {
 
         return water;
     }
+    //Time Complexity: O(n)
+    //Space Complexity: O(1)
+    //Approach: 2 pointer approach
+
+    public int trap_1(int[] height) {
+        int water = 0;
+        int maxi = height[0];
+        int index = 0;
+        int rightMax = height[0];
+        int leftMax = height[height.length - 1];
+
+        for (int i = 1; i < height.length; i++) {
+            if (height[i] > maxi) {
+                maxi = height[i];
+                index = i;
+            }
+        }
+        for (int i = 0; i < index; i++) {
+            rightMax = Math.max(rightMax, height[i]);
+            int diff = Math.min(rightMax, maxi) - height[i];
+            if (diff > 0) {
+                water += diff;
+            }
+        }
+
+        for (int i = height.length - 1; i > index; i--) {
+            leftMax = Math.max(leftMax, height[i]);
+            int diff = Math.min(leftMax, maxi) - height[i];
+            if (diff > 0) {
+                water += diff;
+            }
+        }
+        return water;
+
+    }
 }
 
 public class Trapping_rain_water {
@@ -44,7 +80,7 @@ public class Trapping_rain_water {
     public static void main(String[] args) {
         Solution sol = new Solution();
         int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
-        int result = sol.trap(height);
+        int result = sol.trap_1(height);
         System.out.println(result);
     }
 }
